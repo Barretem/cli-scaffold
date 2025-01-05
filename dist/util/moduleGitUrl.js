6191c1c003d82964048ae3cdf9169428a23e9f77
Barretem <1178310248@qq.com>
2025-01-05 16:35:03 +0800

    feat: remove old cli

--- dist/util/moduleGitUrl.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    'spaas-application-console': 'http://gitlab.deepexi.top/deepexi/spaas-frontend/deepexi-middle-projects/xpaas-application-console.git'
-};
